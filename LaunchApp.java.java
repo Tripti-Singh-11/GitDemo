e85ac7bba25f7d0cc0e0cddf240d696ed7f3e4c5
Tripti-Singh-11 <triptisingh1107t@gmail.com>
2024-12-20 11:57:53 +0530

    Modified after cloning

--- LaunchApp.java.java
+++ LaunchApp.java.java
@@ -3,4 +3,11 @@ public static void main(String[] args)
 {
 System.out.println("Exploring Git and GitHub! ");
 }
+	public static void fun()
+          int a=10;
+	  int b =20;
+		sum=(a+b);
+{
+	system.out.println(sum);
+}
 }
